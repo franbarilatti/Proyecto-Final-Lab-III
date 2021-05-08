739526cc905459680ed4d6e6818d3220ee89dca1
Franco <francoagustinbarilatti@hotmail.com>
2021-05-08 13:49:21 -0300

    first commit

---
+++ src/model/User.java
@@ -0,0 +1,11 @@
+package model;
+
+public abstract class User {
+    private String nickName;
+    private String password;
+
+    public User() {
+        nickName = "";
+        password = "";
+    }
+}
