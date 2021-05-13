5213f172d3bf0129a5f6120c030553497a4faaa9
Franco <francoagustinbarilatti@hotmail.com>
2021-05-13 13:26:20 -0300

    add recepcionist and user atributtes

--- src/model/Recepcionist.java
+++ src/model/Recepcionist.java
@@ -1,4 +1,7 @@
 package model;
 
 public class Recepcionist extends User{
+
+    public Recepcionist() {
+    }
 }
--- src/model/User.java
+++ src/model/User.java
@@ -8,4 +8,25 @@ public abstract class User {
         nickName = "";
         password = "";
     }
+
+    public User(String nickName, String password) {
+        this.nickName = nickName;
+        this.password = password;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
